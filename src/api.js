71f7c24b634299eff25fcfdbbb352cb6473bcdd6
Luis Casas <luisscasas45@gmail.com>
2025-05-02 23:12:08 -0600

    FEAT/ Implement login functionality and update API calls

--- src/api.js
+++ src/api.js
@@ -1,45 +1,50 @@
-const API_URL = 'https://dummyjson.com'
+const API_URL = "https://dummyjson.com";
 
 export async function login(username, password) {
-    const res = await fetch(`${API_URL}/auth/login`,{
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            username,
-            password,
-        }),
-    });
-    const json = await res.json();
-    return json.token;
+  const res = await fetch(`${API_URL}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+  });
+
+  const json = await res.json();
+
+  localStorage.setItem("token", json.accessToken);
+  localStorage.setItem("refreshToken", json.refreshToken);
+
+  return json.accessToken;
 }
 
-export async function getProducts(){
-    const token = localStorage.getItem("token")
-    const res = await fetch(`${API_URL}/products`, {
-        method: "GET",
-        headers: {
-            'Authorization': `Bearer ${token}`,
-            'Content-Type': 'application/json'
-        }
-    });
+export async function getProducts() {
+  const token = localStorage.getItem("token");
+  const res = await fetch(`${API_URL}/products`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  });
 
-    const json = await res.json();
+  const json = await res.json();
 
-    return json.products;
+  return json.products;
 }
 
 export async function getProduct(id) {
-    const token = localStorage.getItem("token")
-    const res = await fetch(`${API_URL}/products/${id}`, {
-        method: "GET",
-        headers: {
-            'Authorization': `Bearer ${token}`,
-            'Content-Type': 'application/json'
-        }
-    })
-    
-    const json = await res.json()
-    return json
+  const token = localStorage.getItem("token");
+  const res = await fetch(`${API_URL}/products/${id}`, {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    },
+  });
+
+  const json = await res.json();
+  return json;
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,41 @@
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-//pages
-import LoginPage from './pages/LoginPages'
-import HomePage from './pages/HomePage'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import { Toaster } from "sonner";
+import ProductsPage from "./pages/ProductsPage";
+import PorductDetailPage from "./pages/ProductDetailPage";
+import MainLayout from "./layouts/MainLayout";
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: < HomePage />
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/productos",
+        element: <ProductsPage />,
+      },
+      {
+        path: "/productos/:id",
+        element: <PorductDetailPage />,
+      },
+    ],
   },
   {
-    path: '/login',
-    element: < LoginPage  />
+    path: "/login",
+    element: <LoginPage />,
   },
-  {
-    path: '/productos',
-    element: <P> Productos</P>
-  },
-  {
-    path: '/productos/:id',
-    element: <P> Productos</P>
-  },
+]);
 
-
-])
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
- 
-
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <>
+    <Toaster />
+    <RouterProvider router={router} />
+  </>
+);
